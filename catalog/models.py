2310868c60bc24b03c1017f8981e718e6bd8aa8f
Bastien Deslandes <bastien.deslandes@free.fr>
2023-03-17 08:57:44 +0100

    initialisation

---
+++ catalog/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
